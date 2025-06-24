73a82c695cf3274f2ac3fe9f7d1111b50c1908db
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-06-24 19:05:56 +0700

    Add method to get NAC task and Master Data Oracle

--- JRN-IDP/Program.cs
+++ JRN-IDP/Program.cs
@@ -16,10 +16,36 @@ namespace JRN_IDP
             ProsnapHandler prosnap = new ProsnapHandler();
             APIHandler api = new APIHandler();
             NACHandler NAC = new NACHandler();
-            string code = "";
+            string code = "4";
             if(code == "0")
             {
                 spoHander.UploadFileToProsnap();
+            }
+            else if(code == "NAC TASK")
+            {
+                string token = NintexTaskHandler.GetToken_DaikinNAC();
+                Console.WriteLine(token);
+                string Form_No = "8300214476";
+                var getTaskResponse = NintexTaskHandler.GetTask_ByInstanceID_Async("c5b295e7-e4ec-48d3-9bf9-f941d1964e2d_0_4");
+                var tasks = getTaskResponse.Result;
+                var task = tasks.Tasks.FirstOrDefault(t => t.Name.Contains(Form_No));
+                Console.WriteLine($"Task count: {tasks.Tasks.Count}");
+                foreach(var t in tasks.Tasks)
+                {
+                    Console.WriteLine($"Task Name: {t.Name}");
+                }
+
+                if(task == null)
+                {
+                    Console.WriteLine("No task found");
+                    return;
+                }
+                Console.WriteLine(task.Name);
+                foreach(var assignment in task.TaskAssignments)
+                {
+                    Console.WriteLine($"\nAssignment ID: {assignment.Id}");
+                    Console.WriteLine($"Assignee: {assignment.Assignee}");
+                }
             }
             else if(code == "1")
             {
@@ -32,6 +58,13 @@ namespace JRN_IDP
             else if(code == "3")
             {
                 spoHander.TestGetFile(4);
+            }
+            else if(code == "4")
+            {
+                Task.Run(async () =>
+                {
+                    await api.GetOracleMasterSuppliers();
+                }).Wait();
             }
             else
             {
