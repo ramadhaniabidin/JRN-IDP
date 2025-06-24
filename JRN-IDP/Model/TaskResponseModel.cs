c36fd6d9ba214d3d603d3934a1129e80813ef37b
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-06-24 19:05:24 +0700

    Create TaskResponseModel.cs

---
+++ JRN-IDP/Model/TaskResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRN_IDP.Model
+{
+    public class TaskResponseModel
+    {
+        public List<TaskItemModel> Tasks { get; set; }
+    }
+}
