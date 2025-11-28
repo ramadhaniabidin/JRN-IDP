692c14199232a205c0478cd1424acbe2766595c1
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-11-28 14:54:09 +0700

    remove unused function

--- JRN-IDP/DaikinCloud/DaikinApplication/JS/Commercials/List.js
+++ JRN-IDP/DaikinCloud/DaikinApplication/JS/Commercials/List.js
@@ -302,16 +302,6 @@ app.controller('ctrl', function ($scope, svc) {
         }
     };
 
-    function getParameterByName(name) {
-        const url = window.location.href;
-        name = name.replace(/[\[\]]/g, '\\$&');
-        const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, ' '));
-    };
-
     $scope.getSelectedClass = function (entity) {
         return $scope.isSelected(entity.Item_ID) ? 'selected' : '';
     };
