ed43c15bf66e4de8dcd34d944e0c427ef92e1673
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-12-09 10:32:49 +0700

    updated regular expression in 'SeperatedNumber.js'

--- JRN-IDP/DaikinCloud/DaikinApplication/JS/Common/SeparatedNumber.js
+++ JRN-IDP/DaikinCloud/DaikinApplication/JS/Common/SeparatedNumber.js
@@ -1,29 +1,33 @@
 ﻿var number_input_view = NWF$("div.forms-number .ms-rtestate-field");
 number_input_view.each(function () {
     var currTarget = $(this);
-
-    var number_val = currTarget.text();
-    if (currTarget != "") {
-        number_val = number_val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        currTarget.text(number_val);
+    var number_val = currTarget.text().trim();
+    if (number_val !== "") {
+        currTarget.text(formatNumberWithCommas(number_val));
     }
 
-})
+});
+
+
+function formatNumberWithCommas(x) {
+    if (x === "") return x;
+    const parts = x.split(".");
+    parts[0] = Number(parts[0]).toLocaleString("en-US");
+    return parts.join(".");
+};
 
 var number_input = NWF$("div.forms-number input");
 number_input.each(function () {
     var currTarget = $(this);
-
     var number_val = currTarget.val();
-    if (currTarget != "") {
-        number_val = number_val.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        currTarget.val(number_val);
+
+    if (currTarget !== "") {
+        currTarget.val(formatNumberWithCommas(number_val));
     }
 
     currTarget.on("input", function () {
         var inputValue = currTarget.val();
 
-        // Remove non-numeric characters except for periods and commas
         inputValue = inputValue.replace(/[^0-9.]/g, "");
 
         var decimalCount = (inputValue.match(/\./g) || []).length;
@@ -32,14 +36,7 @@ number_input.each(function () {
             inputValue = parts.shift() + "." + parts.join("");
         }
 
-        // Format the number with commas for thousands
-        var parts = inputValue.split(".");
-
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-        inputValue = parts.join(".");
-
-        currTarget.val(inputValue);
+        currTarget.val(formatNumberWithCommas(inputValue));
     });
 
 });
