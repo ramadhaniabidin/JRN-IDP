92b2956fe38fad5ff0c26c8b53fc3ba6ffdb105b
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2026-05-19 10:05:41 +0700

    update OptionController

--- JRN-IDP/DaikinCloud/DaikinBusinessLogics/Apps/Master/Controller/OptionController.cs
+++ JRN-IDP/DaikinCloud/DaikinBusinessLogics/Apps/Master/Controller/OptionController.cs
@@ -1,4 +1,5 @@
 ﻿using Daikin.BusinessLogics.Apps.Master.Model;
+using Daikin.BusinessLogics.Apps.Master.Repository;
 using Daikin.BusinessLogics.Common;
 using System;
 using System.Collections.Generic;
@@ -12,34 +13,45 @@ namespace Daikin.BusinessLogics.Apps.Master.Controller
 {
     public class OptionController
     {
-        DatabaseManager db = new DatabaseManager();
+        DatabaseManager db;
         SqlConnection conn = new SqlConnection();
         SqlDataReader reader = null;
         DataTable dt = new DataTable();
+        private readonly string connString = Utility.GetSqlConnection();
+        private readonly OptionRepository repo;
+
+        public OptionController()
+        {
+            db = new DatabaseManager();
+            repo = new OptionRepository(db, connString);
+        }
 
         public List<MasterModuleOptionModel> ModuleTransactionList()
         {
-            //[usp_MasterModule_GetOptionsByTransaction]
-            dt = new DataTable();
-            try
-            {
-                db.OpenConnection(ref conn);
-                db.cmd.CommandText = "dbo.[usp_MasterModule_GetOptionsByTransaction]";
-                db.cmd.CommandType = CommandType.StoredProcedure;
-                db.cmd.Parameters.Clear();
+            return repo.ModuleTransactionList().GetAwaiter().GetResult();
 
-                reader = db.cmd.ExecuteReader();
-                dt.Load(reader);
-                db.CloseDataReader(reader);
-                db.CloseConnection(ref conn);
-                return Utility.ConvertDataTableToList<MasterModuleOptionModel>(dt);
 
-            }
-            catch (Exception ex)
-            {
-                db.CloseConnection(ref conn);
-                throw ex;
-            }
+            ////[usp_MasterModule_GetOptionsByTransaction]
+            //dt = new DataTable();
+            //try
+            //{
+            //    db.OpenConnection(ref conn);
+            //    db.cmd.CommandText = "dbo.[usp_MasterModule_GetOptionsByTransaction]";
+            //    db.cmd.CommandType = CommandType.StoredProcedure;
+            //    db.cmd.Parameters.Clear();
+
+            //    reader = db.cmd.ExecuteReader();
+            //    dt.Load(reader);
+            //    db.CloseDataReader(reader);
+            //    db.CloseConnection(ref conn);
+            //    return Utility.ConvertDataTableToList<MasterModuleOptionModel>(dt);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    db.CloseConnection(ref conn);
+            //    throw ex;
+            //}
         }
 
         public List<MasterModuleOptionModel> ModuleOptions(string SPList)
@@ -140,6 +152,161 @@ namespace Daikin.BusinessLogics.Apps.Master.Controller
             finally
             {
                 db.CloseConnection(ref conn);
+            }
+        }
+
+        public List<BussPlaceModel> GetMasterBussPlace()
+        {
+            DataTable dTable = new DataTable();
+            using (var con = new SqlConnection(connString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("usp_Utility_GetMasterBussPlace", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dTable.Load(reader);
+                        return Utility.ConvertDataTableToList<BussPlaceModel>(dTable);
+                    }
+                }
+            }
+        }
+
+        public async Task<List<BussPlaceModel>> GetMasterBussPlaceAsync()
+        {
+            List<BussPlaceModel> list = new List<BussPlaceModel>();
+            using (SqlConnection _conn = new SqlConnection(connString))
+            {
+                await _conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("usp_Utility_GetMasterBussPlace", _conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader r = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Utility.MapReaderToList<BussPlaceModel>(r).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        public List<OptionModel> GetMasterClaimCategory()
+        {
+            DataTable dTable = new DataTable();
+            using (var con = new SqlConnection(connString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("usp_Utility_GetMasterClaimCategory", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dTable.Load(reader);
+                        return Utility.ConvertDataTableToList<OptionModel>(dTable);
+                    }
+                }
+            }
+        }
+
+        public List<VendorAffiliateModel> GetMasterVendorAffiliate()
+        {
+            DataTable dTable = new DataTable();
+            using (var con = new SqlConnection(connString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("usp_Utility_GetMasterVendorAffiliate", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dTable.Load(reader);
+                        return Utility.ConvertDataTableToList<VendorAffiliateModel>(dTable);
+                    }
+                }
+            }
+        }
+
+        public async Task<List<VendorAffiliateModel>> GetMasterVendorAffiliateAsync()
+        {
+            using (SqlConnection _conn = new SqlConnection(connString))
+            {
+                await _conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("usp_Utility_GetMasterVendorAffiliate", _conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader r = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Utility.MapReaderToList<VendorAffiliateModel>(r);
+                    }
+                }
+            }
+        }
+
+        public List<VendorBankAffiliateModel> GetMasterVendorBankAffiliate()
+        {
+            DataTable dTable = new DataTable();
+            using (var con = new SqlConnection(connString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("usp_GetVendorBankAffiliate", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dTable.Load(reader);
+                        return Utility.ConvertDataTableToList<VendorBankAffiliateModel>(dTable);
+                    }
+                }
+            }
+        }
+
+        public async Task<List<VendorBankAffiliateModel>> GerMasterVendorBankAffiliateAsync()
+        {
+            using (SqlConnection _conn = new SqlConnection(connString))
+            {
+                await _conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("usp_GetVendorBankAffiliate", _conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader r = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Utility.MapReaderToList<VendorBankAffiliateModel>(r);
+                    }
+                }
+            }
+        }
+
+        public List<VendorAffiliateModel> GetMasterCustomerAffiliate()
+        {
+            DataTable dTable = new DataTable();
+            using (var con = new SqlConnection(connString))
+            {
+                con.Open();
+                using (var cmd = new SqlCommand("usp_Utility_GetMasterCustomerAffiliate", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dTable.Load(reader);
+                        return Utility.ConvertDataTableToList<VendorAffiliateModel>(dTable);
+                    }
+                }
+            }
+        }
+
+        public async Task<List<VendorAffiliateModel>> GetMasterCustomerAffiliateAsync()
+        {
+            using (SqlConnection _conn = new SqlConnection(connString))
+            {
+                await _conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("usp_Utility_GetMasterCustomerAffiliate", _conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader r = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Utility.MapReaderToList<VendorAffiliateModel>(r);
+                    }
+                }
             }
         }
 
