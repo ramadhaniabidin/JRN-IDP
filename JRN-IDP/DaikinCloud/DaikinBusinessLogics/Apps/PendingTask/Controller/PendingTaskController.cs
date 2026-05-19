0391c53374d456514d2ae735162a7ecd2517d0c1
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2026-05-19 15:08:23 +0700

    refactor(PendingTaskController): removed commented out code

--- JRN-IDP/DaikinCloud/DaikinBusinessLogics/Apps/PendingTask/Controller/PendingTaskController.cs
+++ JRN-IDP/DaikinCloud/DaikinBusinessLogics/Apps/PendingTask/Controller/PendingTaskController.cs
@@ -15,11 +15,14 @@ namespace Daikin.BusinessLogics.Apps.PendingTask.Controller
 {
     public class PendingTaskController
     {
-        DatabaseManager db = new DatabaseManager();
-        SqlConnection conn = new SqlConnection();
-        SqlDataReader reader = null;
-        DataTable dt = new DataTable();
-        private readonly PendingTaskRepository repo = new PendingTaskRepository();
+        private readonly DatabaseManager db;
+        private readonly PendingTaskRepository repo;
+
+        public PendingTaskController()
+        {
+            db = new DatabaseManager();
+            repo = new PendingTaskRepository(db);
+        }
 
         public async Task<List<PendingTaskModel>> GetPendingTaskListAsync(string CurrentUserEmail, string CurrentLogin, string CurrentUsername, FilterHeaderSearchModel model)
         {
@@ -28,29 +31,8 @@ namespace Daikin.BusinessLogics.Apps.PendingTask.Controller
 
         public int GetCountPendingTask(string CurrentLogin, string CurrentUsername)
         {
-            try
-            {
-                db.OpenConnection(ref conn, true);
-                db.cmd.CommandText = "usp_Utility_CountTasks";
-                db.cmd.CommandType = CommandType.StoredProcedure;
-                db.AddInParameter(db.cmd, "CurrentUserLogin", CurrentLogin);
-                db.AddInParameter(db.cmd, "CurrentUsername", CurrentUsername);
-
-                SqlDataReader reader = db.cmd.ExecuteReader();
-                DataTable dt = new DataTable();
-                dt.Load(reader);
-                db.CloseDataReader(reader);
-                db.CloseConnection(ref conn, true);
-
-                var Count = dt.Rows[0].Field<int>(0);
-
-                return Count;
-            }
-            catch (Exception ex)
-            {
-                db.CloseConnection(ref conn);
-                throw ex;
-            }
+            var result = repo.GetCountPendingTaskAsync(CurrentLogin, CurrentUsername).GetAwaiter().GetResult();
+            return result;
         }
     }
 }
